445bcfa23d4b21f4d9322da3130db062930e3f26
shark <771279601@qq.com>
2018-09-13 22:12:29 +0800

    完成了搜索页面和搜索结果页面的布局，并实现了点击搜索向搜索结果页面传递关键字的功能

---
+++ js/search-result.js
@@ -0,0 +1,13 @@
+$(function(){
+    function getParamsByUrl(url,name) {
+        var params = url.substr(url.indexOf('?')+1).split('&');
+        for(var i=0; i<params.length;i++){
+            var param = params[i].split('=');
+            if (param[0] == name) {
+                return param[1];
+            }
+        }
+        return null;
+    }
+    console.log(getParamsByUrl(location.href,'keyword'));
+})
---
+++ js/search.js
@@ -0,0 +1,26 @@
+$(function(){
+    var keyArr = [];
+    $('.search-btn').on('tap',function(){
+        var keyword = $(this).siblings('input').val();
+        if(keyword.trim()){
+            keyArr.unshift(keyword);
+            localStorage.setItem('keyArr',JSON.stringify(keyArr));
+            location.href = "search-result.html?keyword="+keyword;
+        }else {
+            alert("不能搜索空！");
+        }
+    });
+    if(localStorage.getItem('keyArr')){
+        keyArr = JSON.parse(localStorage.getItem('keyArr'));
+        var html = template('tp',{keyArr:keyArr});
+        $('.mui-table-view').html(html);
+    }
+    $('.clear').on('tap',function(){
+        $('.mui-table-view').html('');
+        localStorage.removeItem('keyArr');
+    })
+    $('.mui-table-view').on('tap','li',function(){
+        var keyword = $(this).children().eq(0).text();
+        location.href = "search-result.html?keyword="+keyword;
+    })
+})
